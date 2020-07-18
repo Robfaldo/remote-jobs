79fa5722ff813162e6a10db33c8c656e4c2d2276
Robert Faldo <robertfaldo@gmail.com>
2020-07-18 13:56:01 +0100

    add Company model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_125427) do
+ActiveRecord::Schema.define(version: 2020_07_18_125540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.date "published_date"
