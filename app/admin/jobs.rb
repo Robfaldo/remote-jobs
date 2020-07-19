5efde2a3e32047b7930f4500f4cef315aa872866
Robert Faldo <robertfaldo@gmail.com>
2020-07-19 16:08:19 +0100

    add permitted params to activeadmin resources

--- app/admin/jobs.rb
+++ app/admin/jobs.rb
@@ -1,18 +1,5 @@
 ActiveAdmin.register Job do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :published_date, :title, :job_link, :level_id, :stack_id, :company_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:published_date, :title, :job_link, :level_id, :stack_id, :company_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :published_date, :title, :job_link, :level_id, :stack_id, :company_id
 end
