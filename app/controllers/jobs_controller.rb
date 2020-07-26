f10bcec10f00643290412c5ac51d3d0b1b554034
Robert Faldo <robertfaldo@gmail.com>
2020-07-26 13:38:37 +0100

    Can filter by stack

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,10 @@
 class JobsController < ApplicationController
   def index
-    levels = Level.names.select{ |name| params[name]["result"] == "1" if params[name] }
+    levels = Level.names.select{ |name| params[name]["is_checked"] == "true" if params[name] }
+    stacks = Stack.names.select{ |name| params[name]["is_checked"] == "true" if params[name] }
 
-    @jobs = Job.joins(:level)
+    @jobs = Job.joins(:level, :stack)
       .merge(Level.search(levels))
+      .merge(Stack.search(stacks))
   end
 end
--- app/models/stack.rb
+++ app/models/stack.rb
@@ -2,4 +2,15 @@ class Stack < ApplicationRecord
   has_many :jobs
 
   validates :name, presence: true
+
+  def self.names
+    Stack.all.distinct.pluck(:name)
+  end
+
+
+  def self.search(name)
+    return all unless name.count > 0
+
+    where(name: name)
+  end
 end
