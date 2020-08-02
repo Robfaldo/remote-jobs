270716adb89f505075f37ff60a0af72fb7ff8fd9
Robert Faldo <robertfaldo@gmail.com>
2020-08-02 14:36:11 +0100

    seed database with base data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# Stacks
+stacks = ["backend", "frontend", "fullstack"]
+stacks.each do |stack|
+  Stack.new(name: stack).save
+end
+
+# Levels
+levels = ["junior", "mid", "senior", "lead", "staff"]
+levels.each do |level|
+  Level.new(name: level).save
+end
+
+# Technologies
+technologies = ["ruby", "javascript", "python", "java", "scala"]
+technologies.each do |technology|
+  Technology.new(name: technology).save
+end
+
+# Companies
+companies = ["test company1", "test company2"]
+companies.each do |company|
+  Company.new(name: company).save
+end
