c8e7f5833892a6c62e2da45fc07fc2529f5f7488
Robert Faldo <robertfaldo@gmail.com>
2020-08-02 18:24:22 +0100

    Add stack methods to stack model (e.g. Stack.backend)

--- app/models/stack.rb
+++ app/models/stack.rb
@@ -8,4 +8,16 @@ class Stack < ApplicationRecord
 
     where(id: ids)
   end
+
+  def self.backend
+    where(name: "backend").first
+  end
+
+  def self.frontend
+    where(name: "frontend").first
+  end
+
+  def self.fullstack
+    where(name: "fullstack").first
+  end
 end
