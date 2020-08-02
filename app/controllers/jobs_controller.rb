dad87efb2fccc817c87fa73d5687b167c7b81624
Robert Faldo <robertfaldo@gmail.com>
2020-08-02 13:00:42 +0100

    Can filter by multiple technologies

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,10 +1,12 @@
 class JobsController < ApplicationController
   def index
+    @technologies = Technology.all
     @levels = Level.names
     @stacks = Stack.names
 
-    @jobs = Job.joins(:level, :stack)
+    @jobs = Job.joins(:level, :stack, :technologies)
       .merge(Level.search(params["levels"]))
       .merge(Stack.search(params["stacks"]))
+      .merge(Technology.search(params["technologies"]))
   end
 end
--- app/models/technology.rb
+++ app/models/technology.rb
@@ -2,4 +2,10 @@ class Technology < ApplicationRecord
   has_and_belongs_to_many :jobs
 
   validates :name, presence: true
+
+  def self.search(name)
+    return all unless name
+
+    where(name: name)
+  end
 end
