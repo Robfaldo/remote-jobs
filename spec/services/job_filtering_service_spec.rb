799cbcd470af16baddf0fcf4a4b7fb5500a49052
Robert Faldo <robertfaldo@gmail.com>
2020-08-04 15:48:28 +0100

    remove Stack stack methods

--- spec/services/job_filtering_service_spec.rb
+++ spec/services/job_filtering_service_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe JobFilteringService do
   end
 
   let(:job_filtering_service) { described_class.new }
+  let(:mid_level) { Level.where(name: "mid").first_or_create }
+  let(:junior_level) { Level.where(name: "junior").first_or_create  }
+  let(:senior_level) { Level.where(name: "senior").first_or_create  }
+  let(:backend) { Stack.where(name: "backend").first_or_create }
+  let(:frontend) { Stack.where(name: "frontend").first_or_create }
+  let(:fullstack) { Stack.where(name: "fullstack").first_or_create }
 
   context 'when filtering by active status' do
     before do
@@ -47,14 +53,14 @@ RSpec.describe JobFilteringService do
 
   context 'when filtering by stacks' do
     before do
-      create_job(stack: Stack.backend, title: 'first job')
-      create_job(stack: Stack.backend, title: 'second job')
-      create_job(stack: Stack.frontend, title: 'third job')
-      create_job(stack: Stack.fullstack)
+      create_job(stack: backend, title: 'first job')
+      create_job(stack: backend, title: 'second job')
+      create_job(stack: frontend, title: 'third job')
+      create_job(stack: fullstack)
     end
 
     it 'returns the jobs with the correct stacks when given one stack' do
-      response = job_filtering_service.call(stacks: [Stack.backend.id])
+      response = job_filtering_service.call(stacks: [backend.id])
 
       expected_response = [
         Job.where(title: 'first job').first,
@@ -66,7 +72,7 @@ RSpec.describe JobFilteringService do
 
     it 'returns the jobs with the correct stacks when given multiple stacks' do
       response = job_filtering_service.call(
-          stacks: [Stack.backend.id, Stack.frontend.id]
+          stacks: [backend.id, frontend.id]
       )
 
       expected_response = [
@@ -89,7 +95,7 @@ RSpec.describe JobFilteringService do
     context 'when given an invalid stack' do
       it 'raises a custom error' do
         expect{  job_filtering_service.call(
-                   stacks: ['invalid stack', Stack.frontend.id]
+                   stacks: ['invalid stack', frontend.id]
                  )
         }.to raise_error(JobFilteringService::InvalidStackProvided)
       end
@@ -98,14 +104,14 @@ RSpec.describe JobFilteringService do
 
   context 'when filtering by levels' do
     before do
-      create_job(level: find_or_create_level("junior"), title: 'first job')
-      create_job(level: find_or_create_level("mid"), title: 'second job')
-      create_job(level: find_or_create_level("mid"), title: 'third job')
-      create_job(level: find_or_create_level("senior"), title: 'fourth job')
+      create_job(level: junior_level, title: 'first job')
+      create_job(level: mid_level, title: 'second job')
+      create_job(level: mid_level, title: 'third job')
+      create_job(level: senior_level, title: 'fourth job')
     end
 
     it 'returns the jobs with the correct levels when given one level' do
-      response = job_filtering_service.call(levels: [find_or_create_level("junior").id])
+      response = job_filtering_service.call(levels: [junior_level.id])
 
       expected_response = [
         Job.where(title: 'first job').first
@@ -117,13 +123,13 @@ RSpec.describe JobFilteringService do
 
   context 'when filtering by multiple things' do
     it 'returns the correct jobs when you search for stack and active' do
-      create_job(active: true, stack: Stack.backend, title: 'first job')
-      create_job(active: false, stack: Stack.backend, title: 'second job')
-      create_job(active: true, stack: Stack.frontend, title: 'third job')
-      create_job(active: false, stack: Stack.fullstack, title: 'fourth job')
+      create_job(active: true, stack: backend, title: 'first job')
+      create_job(active: false, stack: backend, title: 'second job')
+      create_job(active: true, stack: frontend, title: 'third job')
+      create_job(active: false, stack: fullstack, title: 'fourth job')
 
       response = job_filtering_service.call(
-        stacks: [Stack.backend.id, Stack.frontend.id],
+        stacks: [backend.id, frontend.id],
         active: true
       )
 
@@ -139,9 +145,9 @@ end
 
 def create_job(
     active: true,
-    stack: Stack.backend,
+    stack: backend,
     title: 'test title',
-    level: find_or_create_level("mid")
+    level: mid_level
 )
   job = Job.new(
     active: active,
@@ -155,8 +161,4 @@ def create_job(
   job.stack = stack
 
   job.save
-end
-
-def find_or_create_level(level)
-  Level.where(name: level).first_or_create
 end
