6681e097b8cdb4f4e6aa0c25b4caf2bbd6643b8e
Robert Faldo <robertfaldo@gmail.com>
2020-08-04 18:53:10 +0100

    remove search method from Technology

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -2,10 +2,4 @@ class Technology < ApplicationRecord
   has_and_belongs_to_many :jobs
 
   validates :name, presence: true
-
-  def self.search(ids)
-    return all unless ids
-
-    where(id: ids)
-  end
 end
