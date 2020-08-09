c2edd341905553bc6f238edbc423b459e4dd74aa
Robert Faldo <robertfaldo@gmail.com>
2020-08-09 11:39:36 +0100

    Filtered checkboxes remain checked after search

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,12 +4,16 @@ class JobsController < ApplicationController
     @levels = Level.all
     @stacks = Stack.all
 
+    @selected_technologies = params["technologies"]&.map(&:to_i) || []
+    @selected_levels = params["levels"]&.map(&:to_i) || []
+    @selected_stacks = params["stacks"]&.map(&:to_i) || []
+
     job_filtering_service = JobFilteringService.new
 
     @jobs = job_filtering_service.call(
-      levels: params["levels"]&.map(&:to_i) || [],
-      stacks: params["stacks"]&.map(&:to_i) || [],
-      technologies: params["technologies"]&.map(&:to_i) || []
+      levels: @selected_levels,
+      stacks: @selected_stacks,
+      technologies: @selected_technologies
     )
   end
 end
