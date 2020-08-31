8c203ca3373baa85ef5d69d416f5af17472f49c0
Robert Faldo <robertfaldo@gmail.com>
2020-08-31 15:49:23 +0100

    response with created status code if creating job successful

--- app/controllers/create_job_controller.rb
+++ app/controllers/create_job_controller.rb
@@ -4,12 +4,18 @@ class CreateJobController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:create]
 
   def create
-    create_job(
+    job = create_job(
       stack: Stack.where(id: params[:stack].to_i).first,
       title: params[:title],
       link: params[:link],
       company: Company.where(name: params[:company].downcase).first_or_create
     )
-    # TODO: respond to API call
+
+    if job
+      render json: 'created', status: 201
+    else
+      # TODO: much better responses :)
+      render json: 'error, really need to add more logging and better status codes though because you will have no idea what is wrong, sorry.', status: 500
+    end
   end
 end
