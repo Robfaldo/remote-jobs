78fa236e084dcd2e748fd669a88191c6aefeadf4
Robert Faldo <robertfaldo@gmail.com>
2020-12-20 18:53:00 +0000

    remove activeadmin models

--- app/admin/levels.rb
+++
@@ -1,5 +0,0 @@
-ActiveAdmin.register Level do
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  permit_params :name
-end
