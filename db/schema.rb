32ae815345bddb6887d56b9dfb120d791b15e0a6
Robert Faldo <robertfaldo@gmail.com>
2020-12-20 19:53:57 +0000

    add status field to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_195040) do
+ActiveRecord::Schema.define(version: 2020_12_20_195314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2020_12_20_195040) do
     t.string "job_board"
     t.string "source"
     t.string "source_id"
+    t.string "status"
   end
 
 end
