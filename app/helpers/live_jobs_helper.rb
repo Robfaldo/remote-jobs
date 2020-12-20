bf301df6076f300398927af4c0050a6d2e977bc4
Robert Faldo <robertfaldo@gmail.com>
2020-12-20 20:58:09 +0000

    homepage shows basic live jobs

---
+++ app/helpers/live_jobs_helper.rb
@@ -0,0 +1,2 @@
+module LiveJobsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'active_jobs#index'
+  # root 'active_jobs#index'
   post '/create_job' => "create_job#create"
   get '/search' => "search#index"
+  root 'live_jobs#index'
 end
