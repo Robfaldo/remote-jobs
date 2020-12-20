9be854f4b6a369b604e0baa33e847a7bcd638384
Robert Faldo <robertfaldo@gmail.com>
2020-12-20 21:52:37 +0000

    Scraping jobs saves them as Jobs

--- app/services/scraping/scrape_jobs_service.rb
+++ app/services/scraping/scrape_jobs_service.rb
@@ -15,7 +15,24 @@ module Scraping
         jobs.concat(new_jobs)
       end
 
-      jobs
+      jobs.each do |job|
+        next if Job.where(job_link: job.link).count > 0
+
+        new_job = Job.new(
+          title: job.title,
+          job_link: job.link,
+          location: job.location,
+          description: job.description,
+          source: job.source,
+          status: "scraped",
+          company: job.company
+        )
+
+        new_job.source_id = job.source_id if job.source_id
+        new_job.job_board = job.job_board if job.job_board
+
+        new_job.save!
+      end
     end
   end
 end
