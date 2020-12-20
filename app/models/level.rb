18bfffadd16c423f92881362dc987e343d1d2519
Robert Faldo <robertfaldo@gmail.com>
2020-12-20 18:46:11 +0000

    remove old models

--- app/models/level.rb
+++
@@ -1,5 +0,0 @@
-class Level < ApplicationRecord
-  has_many :jobs
-
-  validates :name, presence: true
-end
--- app/models/stack.rb
+++
@@ -1,5 +0,0 @@
-class Stack < ApplicationRecord
-  has_many :jobs
-
-  validates :name, presence: true
-end
