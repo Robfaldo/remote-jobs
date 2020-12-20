6ff3338a210cdd6d9fc8c560f319435f413ae5df
Robert Faldo <robertfaldo@gmail.com>
2020-12-20 20:02:42 +0000

    add new validations for Job

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,4 +6,9 @@ class Job < ApplicationRecord
   validates :title, presence: true
   validates :job_link, presence: true
   validates :location, presence: true
+  validates :source, inclusion: { in: %w(indeed google stackoverflow),
+    message: "%{value} is not a valid source" }
+  validates :description, presence: true
+  validates :status, inclusion: { in: %w(scraped rejected approved),
+    message: "%{value} is not a valid status" }
 end
