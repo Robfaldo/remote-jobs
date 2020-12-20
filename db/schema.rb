53081d7811bc1b1205eb36e406036066fab396f5
Robert Faldo <robertfaldo@gmail.com>
2020-12-20 20:07:04 +0000

    remove active from Job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_200354) do
+ActiveRecord::Schema.define(version: 2020_12_20_200609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2020_12_20_200354) do
     t.string "job_link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "active", default: true, null: false
     t.string "location"
     t.decimal "longitude"
     t.decimal "latitude"
