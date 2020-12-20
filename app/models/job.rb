5a6dbd9e018abdfde147f4f8fde51b70882dde62
Robert Faldo <robertfaldo@gmail.com>
2020-12-20 19:16:57 +0000

    update Job model to remove associations

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,14 +2,8 @@ class Job < ApplicationRecord
   geocoded_by :location
   after_validation :geocode
 
-  has_and_belongs_to_many :technologies
-  belongs_to :level
-  belongs_to :stack
-  belongs_to :company
-
   validates :published_date, presence: true
   validates :title, presence: true
   validates :job_link, presence: true
   validates :location, presence: true
-  validates :degree_required, inclusion: { in: [ true, false ] } # can't use presence: true (because false boolean would fail this check)
 end
