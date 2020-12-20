c6c0ba8d43ea1a5716050219666c6014f3e04ffc
Robert Faldo <robertfaldo@gmail.com>
2020-12-20 20:11:00 +0000

    remove job model publsihed_date validation

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,6 @@ class Job < ApplicationRecord
   geocoded_by :location
   after_validation :geocode
 
-  validates :published_date, presence: true
   validates :title, presence: true
   validates :job_link, presence: true
   validates :location, presence: true
