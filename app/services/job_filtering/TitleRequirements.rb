5ebb1c8f2dc6dbf54de1d520284ad5bfb5dbfa3b
Robert Faldo <robertfaldo@gmail.com>
2020-12-23 20:22:01 +0000

    add TitleRequirements to filtering

---
+++ app/services/job_filtering/TitleRequirements.rb
@@ -0,0 +1,16 @@
+module JobFiltering
+  class TitleRequirements < BaseHandler
+    private
+
+    def handle(job)
+      reject_job(job, "Rejected for missing title requirements. Levels matched: #{@levels_matched}. Roles matched: #{@roles_matched}")
+    end
+
+    def can_handle?(job)
+      @levels_matched = rules["level"].filter { |rule| job.title.downcase.include?(rule.downcase) }
+      @roles_matched = rules["role"].filter { |rule| job.title.downcase.include?(rule.downcase) }
+
+      @levels_matched.count == 0 || @roles_matched.count == 0
+    end
+  end
+end
