78b93391e11710c0e091c410beb1294cb1da4c3c
Robert Faldo <robertfaldo@gmail.com>
2020-12-23 17:20:46 +0000

    add black list

--- app/services/job_filtering/filter_jobs.rb
+++ app/services/job_filtering/filter_jobs.rb
@@ -1,6 +1,6 @@
 module JobFiltering
   class FilterJobs
-    FILTERS = [:current_students_only, :graduates_only, :requires_experience]
+    FILTERS = [:current_students_only, :graduates_only, :requires_experience, :wrong_job_type]
 
     def initialize(jobs)
       @jobs = jobs
@@ -11,11 +11,19 @@ module JobFiltering
         yaml_path = Rails.root.join("config", "filter_rules", "#{filter.to_s}.yml")
         rules = YAML.load(File.read(yaml_path))
         white_list_rules = YAML.load(File.read(Rails.root.join("config", "filter_rules", "white_list.yml")))
+        black_list_rules = YAML.load(File.read(Rails.root.join("config", "filter_rules", "black_list.yml")))
 
         jobs.each do |job|
+          black_list_violations = get_black_list_violations(black_list_rules, job)
+
+          if black_list_violations.count > 0
+            reject_job_with_rejection_note(job, black_list_violations)
+          end
+
+          white_list_matches = get_white_list_matches(white_list_rules, job)
+
           title_rule_violations = get_title_violations(rules, job)
           description_rule_violations = get_description_violations(rules, job)
-          white_list_matches = get_white_list_matches(white_list_rules, job)
 
           if white_list_matches.count > 0
             approve_job_with_whitelist_note(job, white_list_matches)
@@ -66,9 +74,19 @@ module JobFiltering
       matches
     end
 
+    def get_black_list_violations(rules, job)
+      rules["job_link"].filter { |rule| job.job_link.downcase.include?(rule.downcase) }
+    end
+
     def approve_job_with_whitelist_note(job, white_list_matches)
       job.status = "approved"
       job.status_reason = "White listed: #{white_list_matches}"
+    end
+
+    def reject_job_with_rejection_note(job, violations)
+      job.status = "rejected"
+
+      job.status_reason = "Black listed: #{violations}."
     end
 
     attr_reader :jobs
