22b18f4467521204ea5b1cf3b5c39954ee3a916e
Robert Faldo <robertfaldo@gmail.com>
2020-12-23 14:19:27 +0000

    add white list filter to jobs

--- app/services/job_filtering/filter_jobs.rb
+++ app/services/job_filtering/filter_jobs.rb
@@ -10,25 +10,19 @@ module JobFiltering
       FILTERS.each do |filter|
         yaml_path = Rails.root.join("config", "filter_rules", "#{filter.to_s}.yml")
         rules = YAML.load(File.read(yaml_path))
+        white_list_rules = YAML.load(File.read(Rails.root.join("config", "filter_rules", "white_list.yml")))
 
         jobs.each do |job|
           title_rule_violations = get_title_violations(rules, job)
           description_rule_violations = get_description_violations(rules, job)
+          white_list_matches = get_white_list_matches(white_list_rules, job)
 
-          if title_rule_violations.count > 0 || description_rule_violations.count > 0
-            job.status = "rejected"
-
-            if title_rule_violations.count > 0
-              job.status_reason = "" unless job.status_reason
-              job.status_reason << "Title violated the #{filter} filter - rule(s) violated were: #{title_rule_violations}."
-            end
-
-            if description_rule_violations.count > 0
-              job.status_reason = "" unless job.status_reason
-              job.status_reason << "Description violated the #{filter} filter - rule(s) violated were: #{description_rule_violations}."
-            end
+          if white_list_matches.count > 0
+            approve_job_with_whitelist_note(job, white_list_matches)
+          elsif title_rule_violations.count > 0 || description_rule_violations.count > 0
+            reject_job(title_rule_violations, description_rule_violations, job, filter)
           else
-            job.status = "approved" if job.status == "scraped" # we don't want to overwrite jobs already rejected by previous filters
+            approve_job(job)
           end
 
           job.save!
@@ -38,12 +32,43 @@ module JobFiltering
 
     private
 
+    def reject_job(title_rule_violations, description_rule_violations, job, filter)
+      job.status = "rejected"
+
+      if title_rule_violations.count > 0
+        job.status_reason = "" unless job.status_reason
+        job.status_reason << "Title violated the #{filter} filter - rule(s) violated were: #{title_rule_violations}."
+      end
+
+      if description_rule_violations.count > 0
+        job.status_reason = "" unless job.status_reason
+        job.status_reason << "Description violated the #{filter} filter - rule(s) violated were: #{description_rule_violations}."
+      end
+    end
+
+    def approve_job(job)
+      job.status = "approved" if job.status == "scraped" # we don't want to overwrite jobs already rejected by previous filters
+    end
+
     def get_title_violations(rules, job)
       rules["title"].filter { |rule| job.title.downcase.include?(rule.downcase) }
     end
 
     def get_description_violations(rules, job)
       rules["description"].filter { |rule| job.description.downcase.include?(rule.downcase) }
+    end
+
+    def get_white_list_matches(rules, job)
+      matches = []
+      matches.concat(rules["description"].filter { |rule| job.description.downcase.include?(rule.downcase) })
+      matches.concat(rules["title"].filter { |rule| job.title.downcase.include?(rule.downcase) })
+
+      matches
+    end
+
+    def approve_job_with_whitelist_note(job, white_list_matches)
+      job.status = "approved"
+      job.status_reason = "White listed: #{white_list_matches}"
     end
 
     attr_reader :jobs
