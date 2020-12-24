9990c29bb9d2c8b7cff610d866d5e2650f1d8aa7
Robert Faldo <robertfaldo@gmail.com>
2020-12-24 16:00:21 +0000

    update the job count when selecting filter

--- app/javascript/job_filtering.js
+++ app/javascript/job_filtering.js
@@ -1,51 +1,58 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const highlightFilter = function (id) {
-        let filters = document.getElementsByClassName('filter')
+    const selectFilter = function (id, visible_jobs_count) {
+        let filters = document.getElementsByClassName('filter');
 
         for (let i = 0; i < filters.length; i++) {
-            filters[i].style.backgroundColor = "#4CAF50"
-        }
+            filters[i].style.backgroundColor = "#4CAF50";
+        };
 
-        document.getElementById(id).style.backgroundColor = "#89f58d"
+        document.getElementById(id).style.backgroundColor = "#89f58d";
+        document.getElementsByClassName('jobs-count')[0].textContent = visible_jobs_count;
     };
 
     document.getElementById('posted-today').addEventListener('click', function() {
         let jobs = document.getElementsByClassName('job');
+        let visible_jobs_count = 0;
 
         for (let i = 0; i < jobs.length; i++) {
             if (jobs[i].classList.contains('posted-today')) {
                 jobs[i].style.visibility = "visible";
+                visible_jobs_count++
             } else {
                 jobs[i].style.visibility = "hidden";
-            }
-        }
+            };
+        };
 
-        document.getElementById('posted-today').style.backgroundColor = "#89f58d"
+        document.getElementById('posted-today').style.backgroundColor = "#89f58d";
 
-        highlightFilter('posted-today')
+        selectFilter('posted-today', visible_jobs_count);
     });
 
     document.getElementById('posted-three-days').addEventListener('click', function() {
         let jobs = document.getElementsByClassName('job');
+        let visible_jobs_count = 0;
 
         for (let i = 0; i < jobs.length; i++) {
             if (jobs[i].classList.contains('posted-today') || jobs[i].classList.contains('posted-three-days')) {
                 jobs[i].style.visibility = "visible";
+                visible_jobs_count++
             } else {
                 jobs[i].style.visibility = "hidden";
-            }
-        }
+            };
+        };
 
-        highlightFilter('posted-three-days')
+        selectFilter('posted-three-days', visible_jobs_count);
     });
 
     document.getElementById('show-all-jobs').addEventListener('click', function() {
         let jobs = document.getElementsByClassName('job');
+        let visible_jobs_count = 0;
 
         for (let i = 0; i < jobs.length; i++) {
             jobs[i].style.visibility = "visible";
-        }
+            visible_jobs_count++;
+        };
 
-        highlightFilter('show-all-jobs')
+        selectFilter('show-all-jobs', visible_jobs_count);
     });
 });
