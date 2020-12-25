09c9e8a1686be0052cc08c2f780e2ea257fefb16
Robert Faldo <robertfaldo@gmail.com>
2020-12-25 13:04:37 +0000

    add software indicators to message onno filter step

--- app/services/job_filtering/TitleRequirements.rb
+++ app/services/job_filtering/TitleRequirements.rb
@@ -3,15 +3,15 @@ module JobFiltering
     private
 
     def handle(job)
-      reject_job(job, "Rejected for missing title requirements. Levels matched: #{@levels_matched}. Roles matched: #{@roles_matched}")
+      reject_job(job, "Rejected for missing title requirements. Levels matched: #{@levels_matched}. Roles matched: #{@roles_matched}. Software indicators matched: #{@software_indicators_matched}")
     end
 
     def can_handle?(job)
       @levels_matched = rules["level"].filter { |rule| job.title.downcase.include?(rule.downcase) }
       @roles_matched = rules["role"].filter { |rule| job.title.downcase.include?(rule.downcase) }
-      @software_indicator_matched = rules["software_indicator"].filter { |rule| job.title.downcase.include?(rule.downcase) }
+      @software_indicators_matched = rules["software_indicator"].filter { |rule| job.title.downcase.include?(rule.downcase) }
 
-      @levels_matched.count == 0 || @roles_matched.count == 0 || @software_indicator_matched.count == 0
+      @levels_matched.count == 0 || @roles_matched.count == 0 || @software_indicators_matched.count == 0
     end
   end
 end
