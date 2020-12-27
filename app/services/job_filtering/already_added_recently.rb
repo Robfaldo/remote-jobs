492036516d37cf48dba2b2963bddc9d0beec550b
Robert Faldo <robertfaldo@gmail.com>
2020-12-27 11:28:34 +0000

    AlreadyAddedRecently filter uses company/location/title too

--- app/services/job_filtering/already_added_recently.rb
+++ app/services/job_filtering/already_added_recently.rb
@@ -3,12 +3,15 @@ module JobFiltering
     private
 
     def handle(job)
-      reject_job(job, message: "Rejected: Job link has already been added within 1 week, job link: #{job.job_link}")
+      reject_job(job, message: "Rejected: Job link has already been added within 1 week. existing_job_links: #{@existing_job_links}. jobs_with_identical_data: #{@jobs_with_identical_data}")
     end
 
     def can_handle?(job)
+      @existing_job_links = Job.where('created_at >= ?', 1.week.ago).where(job_link: job.job_link)
+      @jobs_with_identical_data = Job.where('created_at >= ?', 1.week.ago).where(company: job.company, title: job.title, location: job.location)
+
       # If there's only 1 then it's the current jobs' one, any more means it's already been added in the last week
-      Job.where('created_at >= ?', 1.week.ago).where(job_link: job.job_link).count > 1
+      @existing_job_links.count > 1 || @jobs_with_identical_data.count > 1
     end
   end
 end
