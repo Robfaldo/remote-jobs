59908eabf4d6f03fa5f87fac1380237b68068f8c
Robert Faldo <robertfaldo@gmail.com>
2020-12-27 11:46:17 +0000

    AlreadyAddedRecently only counts it as duplicate if status is not scraped
    Without this it won't add either of the jobs (the onet hat already exists
     or the one being evaluated because it will mark all as rejected)

--- app/services/job_filtering/already_added_recently.rb
+++ app/services/job_filtering/already_added_recently.rb
@@ -3,15 +3,15 @@ module JobFiltering
     private
 
     def handle(job)
-      reject_job(job, message: "Rejected: Job link has already been added within 1 week. existing_job_links: #{@existing_job_links}. jobs_with_identical_data: #{@jobs_with_identical_data}")
+      reject_job(job, message: "Rejected: Job link has already been added within 1 week. existing_job_links: #{job.link}. jobs_with_identical_data, title:  #{job.title}, company: #{job.company}, location: #{job.location}")
     end
 
     def can_handle?(job)
-      @existing_job_links = Job.where('created_at >= ?', 1.week.ago).where(job_link: job.job_link)
-      @jobs_with_identical_data = Job.where('created_at >= ?', 1.week.ago).where(company: job.company, title: job.title, location: job.location)
+      @identical_links_already_filtered = Job.where('created_at >= ?', 1.week.ago).where(job_link: job.job_link).to_a.filter{|job| job.status != "scraped"}
+      @identical_data_already_filtered = Job.where('created_at >= ?', 1.week.ago).where(company: job.company, title: job.title, location: job.location).to_a.filter{|job| job.status != "scraped"}
 
       # If there's only 1 then it's the current jobs' one, any more means it's already been added in the last week
-      @existing_job_links.count > 1 || @jobs_with_identical_data.count > 1
+      @identical_links_already_filtered.count > 1 || @identical_data_already_filtered.count > 1
     end
   end
 end
