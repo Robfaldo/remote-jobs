fbd54615c4ef52372c136823fbb03fc53b10cf53
Robert Faldo <robertfaldo@gmail.com>
2020-12-30 17:54:13 +0000

    scrape indeed (not rss)

--- app/services/scraping/indeed_scraper.rb
+++ app/services/scraping/indeed_scraper.rb
@@ -1,6 +1,3 @@
-require 'simple-rss'
-require 'open-uri'
-
 module Scraping
   class IndeedScraper < Scraper
     LOCATIONS = ["London"]
@@ -8,43 +5,59 @@ module Scraping
     def get_jobs
       LOCATIONS.each do |location|
         search_links[location].each do |link|
-          jobs_from_rss = SimpleRSS.parse open(link)
+          scraped_page = scrape_page(link: link, wait_time: 10000)
 
-          extract_and_save_job(jobs_from_rss)
+          scraped_jobs = scraped_page.search('.jobsearch-SerpJobCard')
+
+          jobs_to_evaluate = extract_jobs_to_evaluate(scraped_jobs)
+
+          jobs_to_scrape = evaluate_jobs(jobs_to_evaluate)
+
+          extract_and_save_job(jobs_to_scrape)
         end
       end
     end
 
     private
 
-    def extract_and_save_job(jobs_from_rss)
-      jobs_from_rss.items.each do |job|
-        unless Job.where(source_id: job[:guid]).count > 0
-          link = CGI::unescapeHTML(job[:link])
-          scraped_description = scrape_job_description(link)
-          rss_title = CGI::unescapeHTML(job[:title])
+    def extract_jobs_to_evaluate(scraped_all_jobs_page)
+      jobs_to_evaluate = []
 
-          new_job = Job.new(
-              title: rss_title.split(" - ")[0],
-              job_link: link,
-              location: rss_title.split(" - ")[2].split(', ')[0],
-              description: scraped_description,
-              source: :indeed,
-              status: "scraped",
-              company: rss_title.split(" - ")[1],
-              job_board: "Indeed",
-              source_id: job[:guid]
-          )
+      scraped_all_jobs_page.each do |job|
+        scraped_link = job.search('.title').search('a').first.get_attribute('href')
+        link = 'https://www.indeed.co.uk/viewjob' + scraped_link.gsub('/rc/clk', '')
+        company = job.search('.company').search('a').text.strip
+        title = job.search('.title').search('a').text.strip
+        location = job.search('.location').text
 
-          new_job.save!
-        end
+        jobs_to_evaluate.push(JobToEvaluate.new(title: title, link: link, company: company, location: location))
       end
+
+      jobs_to_evaluate
     end
 
-    def scrape_job_description(link)
-      page = scrape_page(link: link)
+    def extract_and_save_job(evaluated_jobs)
+      evaluated_jobs.each do |job|
+        next if Job.where(job_link: job.link).count > 0
 
-      page.search(".jobsearch-jobDescriptionText").text
+        scraped_job_page = scrape_page(link: job.link)
+
+        description = scraped_job_page.search('#jobDescriptionText').text
+
+        new_job = Job.new(
+            title: job.title,
+            job_link: job.link,
+            location: job.location,
+            description: description,
+            source: :indeed,
+            status: "scraped",
+            company: job.company,
+            job_board: "Indeed",
+            source_id: job.link
+        )
+
+        new_job.save!
+      end
     end
   end
 end
