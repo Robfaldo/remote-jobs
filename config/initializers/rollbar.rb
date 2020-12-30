30c61c33e3933a48187951a73fbac54a493a3c95
Robert Faldo <robertfaldo@gmail.com>
2020-12-30 19:56:09 +0000

    add extra data to rollbar stack trace

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -8,6 +8,9 @@ Rollbar.configure do |config|
     config.enabled = false
   end
 
+  config.send_extra_frame_data = :app # or :all
+  config.locals = { :enabled => true }
+
   # By default, Rollbar will try to call the `current_user` controller method
   # to fetch the logged-in user object, and then call that object's `id`
   # method to fetch this property. To customize:
