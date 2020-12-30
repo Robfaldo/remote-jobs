c16ac561702965f6668fc60b37db3b63701a8f5d
Robert Faldo <robertfaldo@gmail.com>
2020-12-30 15:28:40 +0000

    FILTER JAVASCRIPT WORKING!!!!!

--- app/javascript/job_filtering.js
+++ app/javascript/job_filtering.js
@@ -1,111 +1,100 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const tagsMapping = {
-        'requires-experience-filter': 'requires-experience',
-        'requires-stem-degree-filter': 'requires-stem-degree'
+    const dateRangeCheckboxes = document.getElementsByClassName('js-date-range-checkbox');
+    const dateRangeCheckboxesArray = [].slice.call(dateRangeCheckboxes);
+
+    const tagCheckboxes =  document.getElementsByClassName('js-experience-tag-checkbox');
+    const tagCheckboxesArray = [].slice.call(tagCheckboxes);
+
+    const possibleJobDateRanges = ["js-posted-today", "js-posted-three-days", "js-posted-over-three-days"];
+    const filtersMapping = {
+        'js-posted-today': ['js-posted-today'],
+        'js-posted-three-days': ['js-posted-three-days'],
+        'js-show-all-jobs': ['js-posted-today', 'js-posted-over-three-days', 'js-posted-three-days']
     };
 
-    const dateRangeCheckBoxes = ['posted-today', 'show-all-jobs', 'posted-three-days'];
+    for (let i = 0; i < dateRangeCheckboxes.length; i++) {
+        dateRangeCheckboxes[i].addEventListener('click', updateDateRange, false);
+    }
 
-    let checkboxes = document.getElementsByClassName('filter-box');
+    tagCheckboxesArray.forEach(function(checkbox){
+        checkbox.addEventListener('click', updateTag, false)
+    });
 
-    for (let i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener('click', update, false);
-    };
+    function updateDateRange() {
+        updateDateRangeFilters(this, dateRangeCheckboxesArray);
+        updateJobs();
+    }
 
-    function update() {
-        if (dateRangeClicked(this)) {
-            updateDateRangeFilters(this);
-        } else {
-            updateTagsFilters(this);
-        }
+    function updateTag() {
+        updateDateTag(this);
+        updateJobs();
+    }
 
-        let jobsNodeList = document.getElementsByClassName('job');
-        let jobElements = [].slice.call(jobsNodeList);
+    function updateJobs() {
+        let jobsNodeList = document.getElementsByClassName('js-job');
+        let jobsArray = [].slice.call(jobsNodeList);
+
+        const selectedDateRange = getSelectedDateRange();
+        const selectedTags = getSelectedTags();
 
         let visibleJobsCount = 0;
-        let tagsRequired = requiredTags();
 
-        const dateFilterToInclude = getDateFiltersToApply();
-        //
-        // let overThree = 0;
-        // let today = 0;
-        // let lastThree = 0;
-        // let dateRangesMatch = 0;
-        // let hasFilterTag = 0;
-
-        jobElements.forEach(function(jobElement){
+        jobsArray.forEach(function(jobElement){
             const job = new Job(jobElement);
 
-            job.updateDateRange(dateFilterToInclude);
-
-            decideIfJobHasTagToFilter(job);
-            // console.log(job.hasMatchingTags)
-
-            if (job.hasFilterTag) {
-                job.updateJobMeetsTagRequirements(tagsRequired)
-                console.log('matching tags: ' + job.hasMatchingTags)
+            if (jobMatchesSelectedDateRange(job, selectedDateRange)) {
+                job.matches_selected_date_range = true;
             }
 
-            // console.log('date range: ' + job.dateRange)
-            // if (job.dateRange == 'posted-today') {
-            //     today++
-            // } else if (job.dateRange == 'posted-over-three-days') {
-            //     overThree++
-            // } else if (job.dateRange == 'posted-three-days') {
-            //     lastThree++
-            // }
-            //
-            // if (job.hasFilterTag) {
-            //     hasFilterTag++
-            // }
-            // if (job.dateRangeSelected) {
-            //     dateRangesMatch++;
-            // }
-
+            if (jobMatchesSelectedTags(job, selectedTags)) {
+                job.matches_selected_tags = true;
+            }
 
             updateJobListing(job);
 
-            if (job.elementVisible) {
+            if (job.element_visible) {
                 visibleJobsCount++;
             }
         });
-
-        // console.log('overThree: ' + overThree)
-        // console.log('today: ' + today)
-        // console.log('lastThree: ' + lastThree)
-        // console.log('dateRangesMatch: ' + dateRangesMatch)
-        // console.log('hasFilterTag: ' + hasFilterTag)
 
         updateJobsFoundCount(visibleJobsCount);
     }
 
     function Job(element) {
-        this.element = element
-        this.tags = getJobTags(element);
-        this.dateRange = getDateRange(element);
-        this.visibility = '';
-        this.dateRangeSelected = false;
-        this.hasFilterTag = false;
-        this.hasMatchingTags = false;
-
-        this.updateDateRange = updateDateRange;
-        this.updateJobMeetsTagRequirements = updateTagsMatch;
+        this.element = element;
+        this.requires_experience = doesJobRequireExperience(this);
+        this.requires_stem_degree = doesJobRequireStemDegree(this);
+        this.date_range = getDateRange(this);
+        this.matches_selected_date_range = false;
+        this.element_visible = false;
+        this.matches_selected_tags = false;
     }
 
-    function updateTagsMatch(selectedTags) {
-        let job = this;
-        let matchedSelectedTags = [];
+    function jobMatchesSelectedDateRange(job, selectedDateRange) {
+        const jobDateRangesAllowed = filtersMapping[selectedDateRange];
 
-        selectedTags.forEach(function(selectedTag){
-            job.tags.forEach(function(tag) {
-                if (tag.classList.contains(tagsMapping[selectedTag])) {
-                    matchedSelectedTags.push(selectedTag)
-                }
-            });
-        });
+        return jobDateRangesAllowed.includes(job.date_range);
+    }
 
-        if (ArraysMatch(matchedSelectedTags, selectedTags) && matchedSelectedTags.length == job.tags.length) {
-            this.hasMatchingTags = true;
+    function jobMatchesSelectedTags(job, selectedTags) {
+        let requiredTags = [];
+
+        if (job.requires_experience) {
+            requiredTags.push('js-experience-tag-checkbox');
+        }
+
+        if (job.requires_stem_degree) {
+            requiredTags.push('js-stem-degree-tag-checkbox');
+        }
+
+        if (job.requires_experience && job.requires_stem_degree) {
+            return (ArraysMatch(requiredTags, selectedTags) ? true : false);
+        } else if (job.requires_experience) {
+            return (selectedTags.includes('js-experience-tag-checkbox') ? true : false);
+        } else if (job.requires_stem_degree) {
+            return (selectedTags.includes('js-stem-degree-tag-checkbox') ? true : false);
+        } else {
+            return true;
         }
     }
 
@@ -113,145 +102,83 @@ window.addEventListener('DOMContentLoaded', () => {
         return JSON.stringify(first.sort()) === JSON.stringify(second.sort());
     }
 
-    function dateRangeClicked(elementClicked) {
-        return dateRangeCheckBoxes.includes(elementClicked.id);
+    function getSelectedDateRange() {
+        const dateFiltersElements = document.getElementsByClassName('js-date-range-checkbox');
+        const dateFiltersElementsArray = [].slice.call(dateFiltersElements);
+
+        const dateRangeSelected = dateFiltersElementsArray.filter(element => element.classList.contains('checked-date-filter'))[0].id;
+
+        return dateRangeSelected;
     }
 
-    function getJobTags(job) {
-        let allJobTagsElements = job.querySelectorAll('.tag');
-        let allJobTagsElementsArray = [].slice.call(allJobTagsElements);
-
-        // console.log(allJobTagsElementsArray.length)
-
-        return allJobTagsElementsArray;
-    }
-
-    function getDateRange(job) {
-        const possibleDateRanges = ['posted-today', 'posted-over-three-days', 'posted-three-days'];
-
-        return possibleDateRanges.filter(dateRange => job.classList.contains(dateRange))[0];
-    }
-
-    function updateDateRange(dateFilterSeleted) {
-        if (jobMatchesDateFilter(dateFilterSeleted, this)) {
-            this.dateRangeSelected = true
-        }
-
-    }
-
-    function getDateFiltersToApply() {
-        let dateFiltersElements = document.getElementsByClassName('date-filter');
-        let dateFiltersElementsArray = [].slice.call(dateFiltersElements);
-
-        return dateFiltersElementsArray.filter(element => element.classList.contains('checked-filter'))[0].id;
-    }
-
-    function requiredTags() {
+    function getSelectedTags() {
         let tagsElementsToApply = document.getElementsByClassName('checked-tag');
         let tagsToApplyArray = [].slice.call(tagsElementsToApply);
 
         return tagsToApplyArray.map(element => element.id);
     }
 
-    function jobMatchesDateFilter(dateFilterSeleted, job) {
-        const filtersMapping = {
-            // class of filter : class of job
-            'posted-today': ['posted-today'],
-            'posted-three-days': ['posted-three-days'],
-            'show-all-jobs': ['posted-today', 'posted-over-three-days', 'posted-three-days']
-        };
+    function getDateRange(job) {
+        const dateRangesOnJob = possibleJobDateRanges.filter(dateRange => job.element.classList.contains(dateRange));
 
-        let jobDateRangesThatWouldMatch = filtersMapping[dateFilterSeleted];
-
-        return jobDateRangesThatWouldMatch.includes(job.dateRange)
+        return dateRangesOnJob[0];
     }
 
-    function dateRangeMatches(dateRanges, job) {
-        let matchedFilters = 0;
+    function doesJobRequireStemDegree(job) {
+        let tagOnJob = job.element.querySelector('.js-stem-degree-job-tag');
 
-        dateRanges.forEach(function(dateRange) {
-            if (job.element.classList.contains(dateRange)) {
-                matchedFilters++
+        return (tagOnJob ? true : false);
+    }
+
+    function doesJobRequireExperience(job) {
+        let experienceTagOnJob = job.element.querySelector('.js-experience-job-tag');
+
+        return (experienceTagOnJob ? true : false);
+    }
+
+    function updateDateRangeFilters(dateFilterClicked, dateRangeCheckboxesArray) {
+        // uncheck them all
+        dateRangeCheckboxesArray.forEach(function(checkbox){
+            if (checkbox.classList.contains('checked-date-filter')) {
+                checkbox.classList.remove('checked-date-filter');
             }
         });
 
-        return matchedFilters > 0
+        // Check the box that was clicked
+        dateFilterClicked.classList.add('checked-date-filter');
     }
 
-    function updateDateRangeFilters(dateRangeClicked) {
-        // uncheck them all
-        for (let i = 0; i < dateRangeCheckBoxes.length; i ++) {
-            let checkBox = document.getElementById(dateRangeCheckBoxes[i]);
-            if (checkBox.classList.contains('checked-filter')) {
-                checkBox.classList.remove('checked-filter');
-            }
-        }
-
-        // Check the box that was clicked
-        if (!dateRangeClicked.classList.contains('checked-filter')) {
-            dateRangeClicked.classList.add('checked-filter');
+    function updateDateTag(tagClicked) {
+        // toggle the checkbox
+        if (tagClicked.classList.contains('checked-tag')) {
+            tagClicked.classList.remove('checked-tag');
+        } else {
+            tagClicked.classList.add('checked-tag');
         }
     }
 
     function updateJobListing(job) {
-        if (job.dateRangeSelected) {
-            if (job.hasFilterTag) {
-                if (job.hasMatchingTags) {
-                    makeVisible(job)
-                } else {
-                    makeInvisible(job)
-                }
-            } else {
-                makeVisible(job)
-            }
+        if (job.matches_selected_date_range && job.matches_selected_tags) {
+            makeVisible(job);
         } else {
-            makeInvisible(job)
+            makeInvisible(job);
         }
     }
 
     function makeVisible(job) {
         if (job.element.classList.contains('default-hidden')) {
-            job.element.classList.remove('default-hidden')
+            job.element.classList.remove('default-hidden');
         }
         job.element.style.display = "";
-        job.elementVisible = true;
+        job.element_visible = true;
     }
 
     function makeInvisible(job) {
         job.element.style.display = "none";
-        job.elementVisible = false;
+        job.element_visible = false;
     }
 
     function updateJobsFoundCount(visibleJobsCount) {
         document.getElementsByClassName('jobs-count')[0].textContent = visibleJobsCount;
     }
-
-    function decideIfJobHasTagToFilter(job) {
-        let possibleTags = Object.values(tagsMapping);
-
-        let matchedTags = 0;
-
-        possibleTags.forEach(function(possibleTag) {
-            job.tags.forEach(function(tag) {
-                if (tag.classList.contains(possibleTag)) {
-                    matchedTags++
-                }
-            })
-        });
-
-
-        if (matchedTags > 0) {
-            job.hasFilterTag = true
-        }
-    }
-
-    function updateTagsFilters(elementClicked) {
-        // toggle the checkbox
-        if (elementClicked.classList.contains('checked-tag')) {
-            elementClicked.classList.remove('checked-tag')
-        } else {
-            elementClicked.classList.add('checked-tag')
-        }
-    }
-
 });
