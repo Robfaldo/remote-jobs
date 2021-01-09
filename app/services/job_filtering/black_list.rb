6d21172321cd005d1b81e5ebceaa34e705204801
Robert Faldo <robertfaldo@gmail.com>
2021-01-09 12:05:21 +0000

    Can black list by the company name

--- app/services/job_filtering/black_list.rb
+++ app/services/job_filtering/black_list.rb
@@ -3,16 +3,17 @@ module JobFiltering
     private
 
     def handle(job)
-      reject_job(job,"Black listed: #{@black_list_violations}.")
+      reject_job(job,"Black listed: @black_list_link_violations: #{@black_list_link_violations}. @black_list_company_violations: #{@black_list_company_violations}.")
       job.tag_list.add(tags_yaml["FilterRules"]["black_listed"])
 
       job.save!
     end
 
     def can_handle?(job)
-      @black_list_violations = rules["job_link"].filter { |rule| job.job_link.downcase.include?(rule.downcase) }
+      @black_list_link_violations = rules["job_link"].filter { |rule| job.job_link.downcase.include?(rule.downcase) }
+      @black_list_company_violations = rules["company"].filter { |rule| job.company.downcase.strip.include?(rule.downcase.strip) }
 
-      @black_list_violations.count > 0
+      @black_list_link_violations.count > 0 || @black_list_company_violations.count > 0
     end
   end
 end
