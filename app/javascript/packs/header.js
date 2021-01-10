7976b25f255cae5f1cfc4632184d07992e24720a
Robert Faldo <robertfaldo@gmail.com>
2021-01-10 18:46:14 +0000

    nav list stays at top when you scroll

--- app/javascript/packs/header.js
+++ app/javascript/packs/header.js
@@ -2,8 +2,10 @@ window.addEventListener('turbolinks:load', function() {
     $(".js-burger").click(function () {
         if ($(".js-header-text").is(":hidden")) {
             $('.js-header-text').show()
+            $('.js-main-nav-list').hide()
         } else {
             $('.js-header-text').hide()
+            $('.js-main-nav-list').show()
         }
     });
 });
