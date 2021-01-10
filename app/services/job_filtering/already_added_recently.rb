0b650d585cc7e93cd294a352fea5c0bb457c350f
Robert Faldo <robertfaldo@gmail.com>
2021-01-10 09:58:01 +0000

    AlreadyAdded filter step counts jobs that only arent scraped

--- app/services/job_filtering/already_added_recently.rb
+++ app/services/job_filtering/already_added_recently.rb
@@ -5,13 +5,13 @@ module JobFiltering
     def handle(job)
       reject_job(job, message: "Rejected: Job link has already been added within 1 week. existing_job_links: #{job.job_link}. jobs_with_identical_data, title:  #{job.title}, company: #{job.company}, location: #{job.location}")
       job.tag_list.add(tags_yaml["FilterRules"]["already_added_recently"])
-      job.reviewed = true # There's so many of these and I always just mark as reviewed, might aswell just save the effort and mark reviewed here
+      # job.reviewed = true # There's so many of these and I always just mark as reviewed, might aswell just save the effort and mark reviewed here
       job.save!
     end
 
     def can_handle?(job)
-      @identical_links_already_filtered = Job.where('created_at >= ?', 1.week.ago).where(job_link: job.job_link).to_a.filter{|job| job.status != "scraped"}
-      @identical_data_already_filtered = Job.where('created_at >= ?', 1.week.ago).where(company: job.company, title: job.title, location: job.location).to_a.filter{|job| job.status != "scraped"}
+      @identical_links_already_filtered = Job.where('created_at >= ?', 1.week.ago).where(job_link: job.job_link).to_a.select{|job| job.status != "scraped"}
+      @identical_data_already_filtered = Job.where('created_at >= ?', 1.week.ago).where(company: job.company, title: job.title, location: job.location).to_a.select{|job| job.status != "scraped"}
 
       @identical_links_already_filtered.count > 0 || @identical_data_already_filtered.count > 0
     end
