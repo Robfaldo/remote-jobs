e6806ee8f90285a5075cc8c01708761d320aea91
Robert Faldo <robertfaldo@gmail.com>
2021-01-11 21:08:38 +0000

    remove leftover active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :admin_users, ActiveAdmin::Devise.config
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'live_jobs#index'
