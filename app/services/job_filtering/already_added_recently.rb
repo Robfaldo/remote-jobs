f803ca36dfc29cb0f12c00047485f72c5814317d
Robert Faldo <robertfaldo@gmail.com>
2021-01-11 09:12:49 +0000

    AlreadyRecentlyAdded checks link, description and data

--- app/services/job_filtering/already_added_recently.rb
+++ app/services/job_filtering/already_added_recently.rb
@@ -3,7 +3,8 @@ module JobFiltering
     private
 
     def handle(job)
-      reject_job(job, message: "Rejected: Job link has already been added within 1 week. existing_job_links: #{job.job_link}. jobs_with_identical_data, title:  #{job.title}, company: #{job.company}, location: #{job.location}")
+      reject_job(job, message: "Rejected: Job link has already been added within 1 week. @identical_links_already_approved: #{@identical_links_already_approved}. @identical_description_already_approved: #{@identical_description_already_approved}. @identical_data_already_approved: #{@identical_data_already_approved}")
+      reject_job(job, message: "Rejected: Job link has already been added within 1 week. @identical_links_already_approved: #{@identical_links_already_approved}. @identical_description_already_approved: #{@identical_description_already_approved}. @identical_data_already_approved: #{@identical_data_already_approved}")
       job.tag_list.add(tags_yaml["FilterRules"]["already_added_recently"])
       # job.reviewed = true # There's so many of these and I always just mark as reviewed, might aswell just save the effort and mark reviewed here
       job.save!
@@ -12,8 +13,9 @@ module JobFiltering
     def can_handle?(job)
       @identical_links_already_approved = Job.where('created_at >= ?', 1.week.ago).where(job_link: job.job_link, status: 'approved')
       @identical_description_already_approved = Job.where('created_at >= ?', 1.week.ago).where(description: job.description, status: 'approved')
+      @identical_data_already_approved = Job.where('created_at >= ?', 1.week.ago).where(title: job.title, company: job.company, location: job.location status: 'approved')
 
-      @identical_links_already_approved.count > 0 || @identical_description_already_approved.count > 0
+      @identical_links_already_approved.count > 0 || @identical_description_already_approved.count > 0 || @identical_data_already_approved.count > 0
     end
   end
 end
