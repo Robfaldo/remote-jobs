bead927d493d461cb3e93197ae4ce8af92cd768e
Robert Faldo <robertfaldo@gmail.com>
2021-01-11 18:21:31 +0000

    header doesnt grow when dropping burger menu

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -2,9 +2,11 @@ window.addEventListener('turbolinks:load', function() {
     $(".js-burger").click(function () {
         if ($(".js-header-text").is(":hidden")) {
             $('.js-header-text').show()
+            $('.js-header-container').show()
             $('.js-main-nav-list').hide()
         } else {
             $('.js-header-text').hide()
+            $('.js-header-container').hide()
             $('.js-main-nav-list').show()
         }
     });
