5a61898a902d9667f1ba2bc9d251a3f4c246f85a
Robert Faldo <robertfaldo@gmail.com>
2021-01-12 18:21:00 +0000

    Add description blacklist

--- app/services/job_filtering/black_list.rb
+++ app/services/job_filtering/black_list.rb
@@ -3,7 +3,7 @@ module JobFiltering
     private
 
     def handle(job)
-      reject_job(job,"Black listed: @black_list_link_violations: #{@black_list_link_violations}. @black_list_company_violations: #{@black_list_company_violations}.")
+      reject_job(job,"Black listed: @black_list_link_violations: #{@black_list_link_violations}. @black_list_company_violations: #{@black_list_company_violations}. @black_list_description_violations: #{@black_list_description_violations}")
       job.tag_list.add(tags_yaml["FilterRules"]["black_listed"])
 
       job.save!
@@ -12,8 +12,9 @@ module JobFiltering
     def can_handle?(job)
       @black_list_link_violations = rules["job_link"].filter { |rule| job.job_link.downcase.include?(rule.downcase.strip) }
       @black_list_company_violations = rules["company"].filter { |rule| job.company.downcase.strip.include?(rule.downcase.strip) }
+      @black_list_description_violations = rules["description"].filter { |rule| job.description.downcase.include?(rule.downcase.strip) }
 
-      @black_list_link_violations.count > 0 || @black_list_company_violations.count > 0
+      @black_list_link_violations.count > 0 || @black_list_company_violations.count > 0 || @black_list_description_violations.count > 0
     end
   end
 end
