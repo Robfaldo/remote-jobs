d435dcad3255e58c7b721b2ca5e2957fcf7064a2
Robert Faldo <robertfaldo@gmail.com>
2021-01-13 21:38:09 +0000

    Mark jobs filtered for already being added as reviewed

--- app/services/job_filtering/already_added_recently.rb
+++ app/services/job_filtering/already_added_recently.rb
@@ -5,7 +5,7 @@ module JobFiltering
     def handle(job)
       reject_job(job, message: "Rejected: Job link has already been added within 1 week. @identical_links_already_approved: #{@identical_links_already_approved.to_ary}. @identical_description_already_approved: #{@identical_description_already_approved.to_ary}. @identical_data_already_approved: #{@identical_data_already_approved.to_ary}")
       job.tag_list.add(tags_yaml["FilterRules"]["already_added_recently"])
-      # job.reviewed = true # There's so many of these and I always just mark as reviewed, might aswell just save the effort and mark reviewed here
+      job.reviewed = true # There's so many of these and I always just mark as reviewed, might aswell just save the effort and mark reviewed here
       job.save!
     end
 
