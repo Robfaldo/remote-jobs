ae493b649b2cf2911c7f069ffc4fb4c1152bc317
Robert Faldo <robertfaldo@gmail.com>
2021-01-23 11:51:56 +0000

    refactor header - much simpler

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -2,7 +2,7 @@ window.addEventListener('turbolinks:load', function() {
     $(".js-burger").click(function () {
         if ($(".js-header-text").is(":hidden")) {
             $('.js-header-text').show()
-            $('.js-header-container').show()
+            // $('.js-header-container').show()
             $('.js-main-nav-list').hide()
         } else {
             $('.js-header-text').hide()
