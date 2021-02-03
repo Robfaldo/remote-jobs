ef227cb5944183c7e96b61273771091f7d0c5397
Robert Faldo <robertfaldo@gmail.com>
2021-02-03 09:02:01 +0000

    Move created_at scopes to application record
    so that ScrapedJob can use them too

--- app/models/job.rb
+++ app/models/job.rb
@@ -19,11 +19,6 @@ class Job < ApplicationRecord
   validates :status, inclusion: { in: %w(scraped rejected approved),
     message: "%{value} is not a valid status" }
 
-  scope :created_today, lambda{ where(created_at: Date.today.beginning_of_day..Date.today.end_of_day) }
-  scope :created_last_3_days, lambda{ where(created_at: (Date.today - 3)..Date.today.end_of_day) }
-  scope :created_last_14_days, lambda{ where(created_at: (Date.today - 14)..Date.today.end_of_day) }
-  scope :created_last_week, lambda{ where(created_at: (Date.today - 7)..Date.today.end_of_day) }
-
   scope :remove_jobs_requiring_degree, -> do
     where(requires_stem_degree: false)
   end
