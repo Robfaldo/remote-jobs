1a4bd13652b2638b8b38f011e91e5096302342b7
Robert Faldo <robertfaldo@gmail.com>
2021-02-05 20:51:51 +0000

    scrapingbee scrapers run consecutively

--- app/services/scraping/scrape_jobs_service.rb
+++ app/services/scraping/scrape_jobs_service.rb
@@ -2,27 +2,27 @@ module Scraping
   class ScrapeJobsService
     class ScrapingError < StandardError; end
 
-    SCRAPERS = [
+    SCRAPERS_CONCURRENT = [
       Scraping::CwjobsScraper,
       Scraping::ReedScraper,
       Scraping::JobserveScraper,
       Scraping::CvLibraryScraper,
-      Scraping::GlassdoorScraper,
       Scraping::IndeedScraper,
-      Scraping::StackoverflowScraper,
+      Scraping::StackoverflowScraper
       Scraping::TotaljobsScraper,
       Scraping::TechnojobsScraper,
-      Scraping::GoogleScraper
     ]
 
-    ## Decomissioned
-    # Scraping::CompaniesDirectScraper
-    ################
+    # ones that use ScrapingBee that can't be run concurrently
+    SCRAPERS_CONSECUTIVE = [
+      Scraping::GlassdoorScraper,
+      Scraping::GoogleScraper
+    ]
 
     def call
       time_started = Time.now
 
-      results = Parallel.map(SCRAPERS, in_threads: SCRAPERS.count) do |scraper|
+      results = Parallel.map(SCRAPERS_CONCURRENT, in_threads: SCRAPERS_CONCURRENT.count) do |scraper|
         scraper_start_time = Time.now
 
         scraper.new.get_jobs
@@ -36,6 +36,16 @@ module Scraping
         puts e
 
         Rollbar.error(e)
+      end
+
+      SCRAPERS_CONSECUTIVE.each do |scraper|
+        scraper_start_time = Time.now
+
+        scraper.new.get_jobs
+
+        minutes_to_scrape = (Time.now - scraper_start_time) / 60
+
+        results.push({ scraper: scraper, scraper_start_time: scraper_start_time, minutes_to_scrape: minutes_to_scrape })
       end
 
       total_time_to_scrape = Time.now - time_started
