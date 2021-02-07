edc842f6569351955b4218f0a269dc363b8150ad
Robert Faldo <robertfaldo@gmail.com>
2021-02-07 15:15:27 +0000

    add oauth callbacks to devise user routes;

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do
+    # # https://medium.com/@adamlangsner/google-oauth-rails-5-using-devise-and-omniauth-1b7fa5f72c8e for omniauth
+    # get 'users/sign_in', to: 'users/sessions#new', as: :new_admin_session
+    # get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_admin_session
+
     # See https://github.com/heartcombo/devise/issues/4573#issuecomment-445018377. Workaround for clicking refresh after failed registration
     get '/users', to: 'devise/registrations#new'
   end
