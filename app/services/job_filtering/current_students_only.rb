608323ed617e179a5e443da79a2710b47e72d190
Robert Faldo <robertfaldo@gmail.com>
2021-02-14 14:09:05 +0000

    Fix current students filtering step bug
    Before this it would always break on scraping job because
    they don't have description.
    
    Given we're not using description I just removd it

--- app/services/job_filtering/current_students_only.rb
+++ app/services/job_filtering/current_students_only.rb
@@ -3,16 +3,15 @@ module JobFiltering
     private
 
     def handle(job)
-      reject_job_for_title_or_description_violations(@title_rule_violations, @description_rule_violations, job, class_name_underscored.to_sym)
+      reject_job(job, @title_rule_violations)
       job.tag_list.add(tags_yaml["FilterRules"]["current_students_only"])
       job.save!
     end
 
     def can_handle?(job)
       @title_rule_violations = get_title_violations(rules, job)
-      @description_rule_violations = get_description_violations(rules, job)
 
-      @title_rule_violations.count > 0 || @description_rule_violations.count > 0
+      @title_rule_violations.count > 0
     end
   end
 end
