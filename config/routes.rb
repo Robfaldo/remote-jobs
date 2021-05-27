e0d75c4f949b4e44b5fbc7ee965ddf771ccff530
Robert Faldo <robertfaldo@gmail.com>
2021-05-27 08:15:02 +0100

    homepage is now the placeholder page

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  root 'live_jobs#index'
+  # root 'live_jobs#index'
+  root 'placeholder#index'
   get '/stats' => 'scraping_stats#index'
   get '/jobs_secret' => 'jobs#index'
   get '/source_links' => 'source_links#index'
