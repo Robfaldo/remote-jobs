088eddd7e6f78667596bf934601a339642e37baa
Robert Faldo <robertfaldo@gmail.com>
2021-05-29 15:45:50 +0100

    only seed the technologies database if the table exists

--- config/initializers/seeds.rb
+++ config/initializers/seeds.rb
@@ -1,3 +1,18 @@
 # These are seeds we want to run when we load the app. Do NOT include
 # seeds that we only want in local/test environments
-DatabaseSeeds::Technologies.call
+
+def database_exists?
+  ActiveRecord::Base.connection
+rescue ActiveRecord::NoDatabaseError
+  false
+else
+  true
+end
+
+def table_exists?(name)
+  ActiveRecord::Base.connection.table_exists? name
+end
+
+if database_exists? && table_exists?('technologies')
+  DatabaseSeeds::Technologies.call
+end
