3b26f5611a9247f5320906054d6ddae8ac5e08b8
Robert Faldo <robertfaldo@gmail.com>
2021-05-29 11:58:50 +0100

    Migration for creating job technologies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_104425) do
+ActiveRecord::Schema.define(version: 2021_05_29_105650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,15 @@ ActiveRecord::Schema.define(version: 2021_05_29_104425) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "job_technologies", force: :cascade do |t|
+    t.bigint "job_id"
+    t.bigint "technology_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["job_id"], name: "index_job_technologies_on_job_id"
+    t.index ["technology_id"], name: "index_job_technologies_on_technology_id"
   end
 
   create_table "jobs", force: :cascade do |t|
