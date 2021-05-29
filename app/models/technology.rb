08298f9e122037fa02da8ba2e75ee097bd2c8cc1
Robert Faldo <robertfaldo@gmail.com>
2021-05-29 11:56:15 +0100

    Technology model validates booleans

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -4,8 +4,8 @@ class Technology < ApplicationRecord
 
   validates :name, presence: true
   validates :aliases, presence: true
-  validates :is_language, presence: true
-  validates :is_framework, presence: true
-  validates :used_for_frontend, presence: true
-  validates :used_for_backend, presence: true
+  validates :is_language, inclusion: [true, false]
+  validates :is_framework, inclusion: [true, false]
+  validates :used_for_frontend, inclusion: [true, false]
+  validates :used_for_backend, inclusion: [true, false]
 end
