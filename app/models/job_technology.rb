7b2f4d43b56da0b773e09bee739c6d147f13abb8
Robert Faldo <robertfaldo@gmail.com>
2021-05-29 16:14:41 +0100

    Add title & description matches to job tech

--- app/models/job_technology.rb
+++ app/models/job_technology.rb
@@ -1,4 +1,7 @@
 class JobTechnology < ApplicationRecord
   belongs_to :job
   belongs_to :technology
+
+  validates :title_matches, presence: true
+  validates :description_matches, presence: true
 end
