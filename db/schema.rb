35861c1ada4cb33c644253df212ca846608a0ab8
Robert Faldo <robertfaldo@gmail.com>
2021-05-31 08:37:18 +0100

    job belongs to company

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_071810) do
+ActiveRecord::Schema.define(version: 2021_05_31_072820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,8 @@ ActiveRecord::Schema.define(version: 2021_05_31_071810) do
     t.boolean "requires_experience", default: false
     t.boolean "edited", default: false
     t.string "searched_location"
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
   create_table "scraped_jobs", force: :cascade do |t|
@@ -158,5 +160,6 @@ ActiveRecord::Schema.define(version: 2021_05_31_071810) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jobs", "companies"
   add_foreign_key "taggings", "tags"
 end
