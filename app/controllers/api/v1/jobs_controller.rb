55ea94d69ea173f61d7900a9be24c21a21960079
Robert Faldo <robertfaldo@gmail.com>
2023-05-19 12:18:09 +0100

    jobs controller creates job

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -2,7 +2,23 @@ module Api
   module V1
     class JobsController < ActionController::API
       def create
-        binding.pry
+        job = Job.new(
+          title: params["title"],
+          job_link: params["direct_company_link"],
+          location: params["location"],
+          description: params["description"],
+          source: params["source"],
+          status: "scraped",
+          company: CompanyServices::FindOrCreateCompany.call(params["company"]),
+          scraped_company: params["company"],
+          source_id: params["source_link"],
+          job_board: "cv_library",
+          searched_location: "placeholder" #TODO: remove this
+        )
+        job.save!
+
+        JobTags::TagJobs.new([job]).call
+        JobFiltering::FilterJobs.new([job]).call
       end
     end
   end
