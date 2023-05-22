f48718734da82f1b881d11d7567facadd4f50598
Robert Faldo <robertfaldo@gmail.com>
2023-05-22 16:29:58 +0100

    add job to admin dashboard

---
+++ app/admin/jobs.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Job do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :job_link, :location, :longitude, :latitude, :description, :job_board, :source, :source_id, :status, :company, :status_reason, :salary, :reviewed, :requires_stem_degree, :requires_experience, :edited, :searched_location, :company_id, :scraped_company, :tag_list
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :job_link, :location, :longitude, :latitude, :description, :job_board, :source, :source_id, :status, :company, :status_reason, :salary, :reviewed, :requires_stem_degree, :requires_experience, :edited, :searched_location, :company_id, :scraped_company, :tag_list]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
