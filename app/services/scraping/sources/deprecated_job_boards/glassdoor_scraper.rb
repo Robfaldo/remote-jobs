2bf10eea374bd6387716eabe7909a2b615ada69d
Robert Faldo <robertfaldo@gmail.com>
2023-06-18 11:36:49 +0100

    namespace deprecated job board with sources

--- app/services/scraping/sources/deprecated_job_boards/glassdoor_scraper.rb
+++ app/services/scraping/sources/deprecated_job_boards/glassdoor_scraper.rb
@@ -1,90 +1,92 @@
 module Scraping
-  module DeprecatedJobBoards
-    class GlassdoorScraper < ::Scraping::Sources::Base
-      private
+  module Sources
+    module DeprecatedJobBoards
+      class GlassdoorScraper < ::Scraping::Sources::Base
+        private
 
-      def source
-        :glassdoor
-      end
-
-      def scrape_all_jobs_page_options(link)
-        {
-          link: link,
-          wait_time: 20000,
-          premium_proxy: true
-        }
-      end
-
-      def scrape_job_page_options(job)
-        {
-          link: job.url,
-          javascript_snippet: javascript,
-          wait_time: 10000,
-          premium_proxy: true
-        }
-      end
-
-      def job_element
-        '.react-job-listing'
-      end
-
-      def job_element_title(job)
-        job.search('.jobLink').search('span').last.text
-      end
-
-      def job_element_link(job)
-        'https://www.glassdoor.co.uk' + job.search('.jobLink').first.get_attribute('href')
-      end
-
-      def job_element_company(job)
-        job.search('.jobLink').search('span')[1].text
-      end
-
-      def job_element_location(job)
-        job.search('.e1rrn5ka0').text
-      end
-
-      def create_job(job, scraped_job_page)
-        description = scraped_job_page.search('.desc')[-1].text
-        new_link = scraped_job_page.search('#current-url').first.text # the link changes after this page loads
-
-        if field_empty?(job.company)
-          rating = scraped_job_page.search('.e11nt52q1').first.search('span').first&.text
-          company = scraped_job_page.search('.e11nt52q1').first.text.strip
-          company.gsub(rating, '') if rating # doesn't always have a rating
-        else
-          company = job.company
+        def source
+          :glassdoor
         end
 
-        new_job = Job.new(
-          title: job.title,
-          url: new_link,
-          location: job.location,
-          description: description,
-          source: source,
-          status: "scraped",
-          company: CompanyServices::FindOrCreateCompany.call(company),
-          scraped_company: company,
-          source_id: job.url
-        )
+        def scrape_all_jobs_page_options(link)
+          {
+            link: link,
+            wait_time: 20000,
+            premium_proxy: true
+          }
+        end
 
-        save_job(new_job, scraped_job_page)
-      end
+        def scrape_job_page_options(job)
+          {
+            link: job.url,
+            javascript_snippet: javascript,
+            wait_time: 10000,
+            premium_proxy: true
+          }
+        end
 
-      def javascript
-        %{
-          (function () {
-              setTimeout(function () {
-                  if (document.getElementsByClassName('ecgq1xb2').length > 0) {
-                    document.getElementsByClassName('ecgq1xb2')[0].click(); // click the show more button
-                  };
-                  el = document.createElement('div');
-                  el.id = 'current-url';
-                  el.textContent = window.location.href;
-                  document.body.appendChild(el);
-              }, 5000);
-          })();
-        }
+        def job_element
+          '.react-job-listing'
+        end
+
+        def job_element_title(job)
+          job.search('.jobLink').search('span').last.text
+        end
+
+        def job_element_link(job)
+          'https://www.glassdoor.co.uk' + job.search('.jobLink').first.get_attribute('href')
+        end
+
+        def job_element_company(job)
+          job.search('.jobLink').search('span')[1].text
+        end
+
+        def job_element_location(job)
+          job.search('.e1rrn5ka0').text
+        end
+
+        def create_job(job, scraped_job_page)
+          description = scraped_job_page.search('.desc')[-1].text
+          new_link = scraped_job_page.search('#current-url').first.text # the link changes after this page loads
+
+          if field_empty?(job.company)
+            rating = scraped_job_page.search('.e11nt52q1').first.search('span').first&.text
+            company = scraped_job_page.search('.e11nt52q1').first.text.strip
+            company.gsub(rating, '') if rating # doesn't always have a rating
+          else
+            company = job.company
+          end
+
+          new_job = Job.new(
+            title: job.title,
+            url: new_link,
+            location: job.location,
+            description: description,
+            source: source,
+            status: "scraped",
+            company: CompanyServices::FindOrCreateCompany.call(company),
+            scraped_company: company,
+            source_id: job.url
+          )
+
+          save_job(new_job, scraped_job_page)
+        end
+
+        def javascript
+          %{
+            (function () {
+                setTimeout(function () {
+                    if (document.getElementsByClassName('ecgq1xb2').length > 0) {
+                      document.getElementsByClassName('ecgq1xb2')[0].click(); // click the show more button
+                    };
+                    el = document.createElement('div');
+                    el.id = 'current-url';
+                    el.textContent = window.location.href;
+                    document.body.appendChild(el);
+                }, 5000);
+            })();
+          }
+        end
       end
     end
   end
--- app/services/scraping/sources/deprecated_job_boards/indeed_scraper.rb
+++ app/services/scraping/sources/deprecated_job_boards/indeed_scraper.rb
@@ -1,87 +1,89 @@
 module Scraping
-  module DeprecatedJobBoards
-    class IndeedScraper < ::Scraping::Sources::Base
-      private
+  module Sources
+    module DeprecatedJobBoards
+      class IndeedScraper < ::Scraping::Sources::Base
+        private
 
-      def source
-        :indeed
-      end
-
-      def scrape_all_jobs_page_options(link)
-        {
-          link: link,
-          wait_time: 10000
-        }
-      end
-
-      def scrape_job_page_options(job)
-        {
-          link: job.url
-        }
-      end
-
-      def job_element
-        '.result'
-      end
-
-      def job_element_company(job)
-        job.search('.companyName').text.strip
-      end
-
-      def job_element_title(job)
-        job.search('[@title]').first.text
-      end
-
-      def job_element_location(job)
-        job.search('.companyLocation').text.strip
-      end
-
-      def job_element_link(job)
-        scraped_link = job.search('a').find do |element|
-          element.get_attribute('href') && element.get_attribute('href').include?('rc/clk?')
-        end.get_attribute('href')
-
-        'https://uk.indeed.com/viewjob' + scraped_link.gsub('/rc/clk', '')
-      end
-
-      def handle_pagination
-        false
-      end
-
-      def pages_remaining_to_scrape(scraped_all_jobs_page)
-        jobs_appearing_in_search = scraped_all_jobs_page.search('#searchCountPages').text.strip.gsub('Page 1 of ', '').gsub(' jobs', '').to_i
-        jobs_per_page = 15
-        total_pages_to_scrape = (jobs_appearing_in_search / jobs_per_page.to_f).ceil
-
-        total_pages_to_scrape - 1 # we already scraped the 1st page
-      end
-
-      def paginated_page_link(link, current_paginated_page)
-        # if it's the 1st extra page (2nd total page) start at "&start=10",
-        # 2nd page "&start=20" etc..
-        link + "&start=#{current_paginated_page}0"
-      end
-
-      def create_job(job, scraped_job_page)
-        description = scraped_job_page.search('#jobDescriptionText').text
-
-        if job.company == ""
-          job.company = scraped_job_page.xpath('//div[contains(@class, "companyrating")]').first.text
+        def source
+          :indeed
         end
 
-        new_job = Job.new(
-            title: job.title,
-            url: job.url,
-            location: job.location,
-            description: description,
-            source: source,
-            status: "scraped",
-            company: CompanyServices::FindOrCreateCompany.call(job.company),
-            scraped_company: job.company,
-            source_id: job.url
-        )
+        def scrape_all_jobs_page_options(link)
+          {
+            link: link,
+            wait_time: 10000
+          }
+        end
 
-        save_job(new_job, scraped_job_page)
+        def scrape_job_page_options(job)
+          {
+            link: job.url
+          }
+        end
+
+        def job_element
+          '.result'
+        end
+
+        def job_element_company(job)
+          job.search('.companyName').text.strip
+        end
+
+        def job_element_title(job)
+          job.search('[@title]').first.text
+        end
+
+        def job_element_location(job)
+          job.search('.companyLocation').text.strip
+        end
+
+        def job_element_link(job)
+          scraped_link = job.search('a').find do |element|
+            element.get_attribute('href') && element.get_attribute('href').include?('rc/clk?')
+          end.get_attribute('href')
+
+          'https://uk.indeed.com/viewjob' + scraped_link.gsub('/rc/clk', '')
+        end
+
+        def handle_pagination
+          false
+        end
+
+        def pages_remaining_to_scrape(scraped_all_jobs_page)
+          jobs_appearing_in_search = scraped_all_jobs_page.search('#searchCountPages').text.strip.gsub('Page 1 of ', '').gsub(' jobs', '').to_i
+          jobs_per_page = 15
+          total_pages_to_scrape = (jobs_appearing_in_search / jobs_per_page.to_f).ceil
+
+          total_pages_to_scrape - 1 # we already scraped the 1st page
+        end
+
+        def paginated_page_link(link, current_paginated_page)
+          # if it's the 1st extra page (2nd total page) start at "&start=10",
+          # 2nd page "&start=20" etc..
+          link + "&start=#{current_paginated_page}0"
+        end
+
+        def create_job(job, scraped_job_page)
+          description = scraped_job_page.search('#jobDescriptionText').text
+
+          if job.company == ""
+            job.company = scraped_job_page.xpath('//div[contains(@class, "companyrating")]').first.text
+          end
+
+          new_job = Job.new(
+              title: job.title,
+              url: job.url,
+              location: job.location,
+              description: description,
+              source: source,
+              status: "scraped",
+              company: CompanyServices::FindOrCreateCompany.call(job.company),
+              scraped_company: job.company,
+              source_id: job.url
+          )
+
+          save_job(new_job, scraped_job_page)
+        end
       end
     end
   end
