94144cfb47245dd31f9d8d01ce31d577b8de90ba
Robert Faldo <robertfaldo@gmail.com>
2023-06-21 21:42:09 +0100

    level tagging now working

--- app/services/job_evaluation/tag_steps/level.rb
+++ app/services/job_evaluation/tag_steps/level.rb
@@ -4,18 +4,43 @@ module JobEvaluation
       include EvaluationHelpers::TagStepHelper
 
       def call
-        job.tag_list.add(tags_yaml["Level"]["graduate"]) if job.title.include?("grad")
-        job.tag_list.add(tags_yaml["Level"]["junior"]) if job.title.include?("junior")
-        job.tag_list.add(tags_yaml["Level"]["mid"]) if job.title.include?("mid")
-        job.tag_list.add(tags_yaml["Level"]["senior"]) if job.title.include?("senior")
-        job.tag_list.add(tags_yaml["Level"]["staff"]) if job.title.include?("staff")
-        job.tag_list.add(tags_yaml["Level"]["lead"]) if job.title.include?("lead")
-        job.tag_list.add(tags_yaml["Level"]["principle"]) if job.title.include?("principle")
+        if job_title_includes_any_level_name?
+          add_tag("graduate") if title_includes?("grad")
+          add_tag("junior") if title_includes?("junior")
+          add_tag("mid") if title_includes?("mid")
+          add_tag("senior") if title_includes?("senior")
+          add_tag("staff") if title_includes?("staff")
+          add_tag("lead") if title_includes?("lead")
+          add_tag("principle") if title_includes?("principle")
+        else
+          # assuming this because a "ruby engineer" would assumably be mid level
+          job.tag_list.add(tags_yaml["Level"]["mid"])
+        end
+
         job.save!
       end
 
       def can_handle?
         true
+      end
+
+      private
+
+      def job_title_includes_any_level_name?
+        all_level_names.any? { |level| title_includes?(level) }
+      end
+
+
+      def all_level_names
+        ["graduate", "junior", "mid", "senior", "staff", "lead", "principle"]
+      end
+
+      def add_tag(tag)
+        job.tag_list.add(tags_yaml["Level"][tag])
+      end
+
+      def title_includes?(str)
+        job.title.downcase.include?(str)
       end
     end
   end
