8d214c4d2edbd01b8d0277f088b21431ece9ea73
Robert Faldo <robertfaldo@gmail.com>
2023-06-21 21:23:43 +0100

    remove db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_190847) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_060122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,7 +89,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_190847) do
     t.string "scraped_company"
     t.string "remote_status"
     t.integer "filter_reason"
-    t.integer "level"
     t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
