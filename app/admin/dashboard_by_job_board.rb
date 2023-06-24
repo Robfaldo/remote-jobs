3b3f5aa00e3c865272fc8c38a4db8a215eeef063
Robert Faldo <robertfaldo@gmail.com>
2023-06-24 16:11:55 +0100

    section off each job board

--- app/admin/dashboard_by_job_board.rb
+++ app/admin/dashboard_by_job_board.rb
@@ -6,70 +6,72 @@ ActiveAdmin.register_page "DashboardByJobBoard" do
     columns do
       column do
         ['indeed', 'linkedin'].each do |job_board|
-          panel "#{job_board.upcase} Jobs at each status based on when the job was created" do
-            table_for ["scraped", "filtered", "evaluated", "total"] do
-              column "Status", &:to_s
+          panel job_board.upcase do
+            panel "Jobs at each status based on when the job was created" do
+              table_for ["scraped", "filtered", "evaluated", "total"] do
+                column "Status", &:to_s
 
-              column "Created today" do |status|
-                Job.public_send(status)
-                   .where(created_at: Time.zone.now.beginning_of_day..Time.zone.now)
-                   .where(source: job_board)
-                   .count
-              end
+                column "Created today" do |status|
+                  Job.public_send(status)
+                     .where(created_at: Time.zone.now.beginning_of_day..Time.zone.now)
+                     .where(source: job_board)
+                     .count
+                end
 
-              column "Created past 3 days" do |status|
-                Job.public_send(status)
-                   .where(created_at: 3.days.ago.beginning_of_day..Time.zone.now)
-                   .where(source: job_board)
-                   .count
-              end
+                column "Created past 3 days" do |status|
+                  Job.public_send(status)
+                     .where(created_at: 3.days.ago.beginning_of_day..Time.zone.now)
+                     .where(source: job_board)
+                     .count
+                end
 
-              column "Created past week" do |status|
-                Job.public_send(status)
-                   .where(created_at: 1.week.ago.beginning_of_day..Time.zone.now)
-                   .where(source: job_board)
-                   .count
-              end
+                column "Created past week" do |status|
+                  Job.public_send(status)
+                     .where(created_at: 1.week.ago.beginning_of_day..Time.zone.now)
+                     .where(source: job_board)
+                     .count
+                end
 
-              column "Created past Month" do |status|
-                Job.public_send(status)
-                   .where(created_at: 1.month.ago.beginning_of_day..Time.zone.now)
-                   .where(source: job_board)
-                   .count
+                column "Created past Month" do |status|
+                  Job.public_send(status)
+                     .where(created_at: 1.month.ago.beginning_of_day..Time.zone.now)
+                     .where(source: job_board)
+                     .count
+                end
               end
             end
-          end
 
-          panel "#{job_board.upcase} Job Previews at each status based on when the job preview was created" do
-            table_for ["scraped", "filtered", "evaluated", "total"] do
-              column "Status", &:to_s
+            panel "Job Previews at each status based on when the job preview was created" do
+              table_for ["scraped", "filtered", "evaluated", "total"] do
+                column "Status", &:to_s
 
-              column "Created today" do |status|
-                JobPreview.public_send(status)
-                          .where(created_at: Time.zone.now.beginning_of_day..Time.zone.now)
-                          .where(source: job_board)
-                          .count
-              end
+                column "Created today" do |status|
+                  JobPreview.public_send(status)
+                            .where(created_at: Time.zone.now.beginning_of_day..Time.zone.now)
+                            .where(source: job_board)
+                            .count
+                end
 
-              column "Created past 3 days" do |status|
-                JobPreview.public_send(status)
-                          .where(created_at: 3.days.ago.beginning_of_day..Time.zone.now)
-                          .where(source: job_board)
-                          .count
-              end
+                column "Created past 3 days" do |status|
+                  JobPreview.public_send(status)
+                            .where(created_at: 3.days.ago.beginning_of_day..Time.zone.now)
+                            .where(source: job_board)
+                            .count
+                end
 
-              column "Created past week" do |status|
-                JobPreview.public_send(status)
-                          .where(created_at: 1.week.ago.beginning_of_day..Time.zone.now)
-                          .where(source: job_board)
-                          .count
-              end
+                column "Created past week" do |status|
+                  JobPreview.public_send(status)
+                            .where(created_at: 1.week.ago.beginning_of_day..Time.zone.now)
+                            .where(source: job_board)
+                            .count
+                end
 
-              column "Created past Month" do |status|
-                JobPreview.public_send(status)
-                          .where(created_at: 1.month.ago.beginning_of_day..Time.zone.now)
-                          .where(source: job_board)
-                          .count
+                column "Created past Month" do |status|
+                  JobPreview.public_send(status)
+                            .where(created_at: 1.month.ago.beginning_of_day..Time.zone.now)
+                            .where(source: job_board)
+                            .count
+                end
               end
             end
           end
