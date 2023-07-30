834b7343cf3a61dad41dedd57902e5e15eeaafcc
Robert Faldo <robertfaldo@gmail.com>
2023-07-30 18:11:32 +0100

    only send 50 live jobs

--- app/models/job.rb
+++ app/models/job.rb
@@ -40,7 +40,7 @@ class Job < ApplicationRecord
     with_main_technology("ruby")
       .where(status: :evaluated)
       .order(created_at: :desc)
-      .limit(100)
+      .limit(50)
   end
 
   def filtered?
