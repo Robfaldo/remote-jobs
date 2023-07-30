ad63b4a5b4c70076a5dce088bd7d9acded88b667
Robert Faldo <robertfaldo@gmail.com>
2023-07-30 20:48:17 +0100

    add job locations to dto

--- app/models/job_dto.rb
+++ app/models/job_dto.rb
@@ -1,27 +1,35 @@
 # This is a "data transfer object". Its purpose is to encapsulate data to be sent to somewhere
 class JobDto
-  attr_reader :reference, :title, :location, :longitude, :latitude,
+  attr_reader :reference, :title, :locations, :longitude, :latitude,
               :description, :datetime_posted, :company, :company_id,
-              :remote_status, :url, :levels, :source
+              :remote_status, :url, :levels, :source, :location_on_advert
 
   def initialize(job)
     @reference = job.id
     @title = job.title
-    @location = job.location
+    @locations = locations(job)
+    @location_on_advert = job.location
     @longitude = job.longitude
     @latitude = job.latitude
     @description = job.description
     @datetime_posted = job.created_at
     @company = job.company.name
     @company_id = job.company.id
-    @fully_remote = job.remote_status == "fully_remote"
     @url = job.url
     @levels = get_levels_from_tags(job)
     @source = job.source
-    @in_london = job.london_based?
   end
 
   private
+
+  def locations(job)
+    locations = []
+
+    locations << "london" if job.london_based?
+    locations << "fully remote" if job.remote_status == "fully_remote"
+
+    locations
+  end
 
   def get_levels_from_tags(job)
     all_levels = ["grad", "junior", "mid", "senior", "staff", "lead", "principle"]
