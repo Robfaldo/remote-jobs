8934bb8a43b7f9aff6bbf83c17418cd3cc974901
Robert Faldo <robertfaldo@gmail.com>
2023-08-06 15:57:06 +0100

    remove redundant cleo method

--- app/services/scraping/sources/careers_pages/cleo.rb
+++ app/services/scraping/sources/careers_pages/cleo.rb
@@ -2,10 +2,6 @@ module Scraping
   module Sources
     module CareersPages
       class Cleo < Greenhouse
-        def location_from_job_element(job_element)
-          job_element.at('.location').text.strip
-        end
-
         def sanitized_location(job_element)
           job_element.at('.location').text.strip.gsub(" or remote (U.K)","")
         end
