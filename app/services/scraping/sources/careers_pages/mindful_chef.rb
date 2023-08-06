731c9a72f46e75e71fb6c732bc84ef39aa316028
Robert Faldo <robertfaldo@gmail.com>
2023-08-06 13:49:57 +0100

    add MindfulChef

---
+++ app/services/scraping/sources/careers_pages/mindful_chef.rb
@@ -0,0 +1,31 @@
+module Scraping
+  module Sources
+    module CareersPages
+      class MindfulChef < Base
+        def job_elements(all_jobs_page)
+          all_jobs_page.search('.job-box-link').uniq {|element| element.get_attribute('href') }
+        end
+
+        def title_from_job_element(job_element)
+          job_element.at('.jb-title').text.strip
+        end
+
+        def url_from_job_element(job_element)
+          job_element.get_attribute("href")
+        end
+
+        def location_from_job_element(job_element)
+          job_element.at('.multi-offices-desktop')
+                     .text
+                     .strip
+                     .gsub(/\s*·\s*/, '')
+                     .strip
+                     .gsub("Mindful Chef ", "")
+                     .gsub(" HQ", "")
+                     .gsub("Oaklands international (Redditch)", "Redditch")
+        end
+      end
+    end
+  end
+end
+
--- lib/tasks/scrape_careers_pages.rake
+++ lib/tasks/scrape_careers_pages.rake
@@ -1,5 +1,6 @@
 task :scrape_careers_pages => :environment do
   companies_with_careers_pages = Company.where.not(careers_page_url: nil)
+  # companies_with_careers_pages = [Company.find_by(name: "MindfulChef")]
 
   companies_with_careers_pages.each do |company|
     Scraping::ScrapeCareersPage.new(company: company).call
