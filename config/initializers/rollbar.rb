1995c81a8070d018823b4dfd13cc27080edb6271
Robert Faldo <robertfaldo@gmail.com>
2023-08-08 18:34:20 +0100

    remove the env branch from rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -5,7 +5,7 @@ Rollbar.configure do |config|
   config.access_token = ENV["ROLLBAR_TOKEN"]
 
   if Rails.env.test? || Rails.env.development?
-    config.enabled = false unless ENV["ROLLBAR"]
+    config.enabled = false
   end
 
   # By default, Rollbar will try to call the `current_user` controller method
