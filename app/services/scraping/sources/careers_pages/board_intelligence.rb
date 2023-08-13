a51c3ba042ee73b2cdfda56a17271dd0f40bd3a1
Robert Faldo <robertfaldo@gmail.com>
2023-08-13 21:27:56 +0100

    scrape board intelligence careers page

---
+++ app/services/scraping/sources/careers_pages/board_intelligence.rb
@@ -0,0 +1,16 @@
+module Scraping
+  module Sources
+    module CareersPages
+      class BoardIntelligence < Workable
+        def job_elements(all_jobs_page)
+          all_jobs_page.search('[data-ui="job"]')
+        end
+
+        def title_from_job_element(job_element)
+          job_element.at('[data-ui="job-title"]').text
+        end
+      end
+    end
+  end
+end
+
