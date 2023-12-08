611ac59d00e04f933f495e139000f618a324a551
Rob Faldo <robertfaldo@gmail.com>
2023-12-08 20:02:08 +0000

    make url unqiue on transaction

--- app/models/property_transaction.rb
+++ app/models/property_transaction.rb
@@ -1,5 +1,6 @@
 class PropertyTransaction < ApplicationRecord
   validates :searched_url, :searched_postcode, :url, :address, :date_of_sale, :price, presence: true
+  validates :url, uniqueness: true
 end
 
 
