083ff31f96bf32b2aa2f5706dfaea031a23e8bd7
Rob Faldo <robertfaldo@gmail.com>
2023-12-08 23:36:36 +0000

    remove unqiueness

--- app/models/property_transaction.rb
+++ app/models/property_transaction.rb
@@ -1,6 +1,5 @@
 class PropertyTransaction < ApplicationRecord
   validates :searched_url, :searched_postcode, :url, :address, :date_of_sale, :price, presence: true
-  validates :url, uniqueness: true
 end
 
 
